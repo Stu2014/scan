708e7568591bf5b3774fbf0a9b4a1ebd406d33f0
Stu <50605146+Stu2014@users.noreply.github.com>
2020-06-17 23:39:18 +0800

    Update spfcheck.py

--- spfcheck.py
+++ spfcheck.py
@@ -8,41 +8,41 @@ headers ={"Content-Type": "application/json"}
 
 #检测是否配置spf
 def check_vul(url):
-	global vuldomain
-	try:
-		A = str(dns.resolver.query(url,"txt").response)
-		if A.find("v=spf") >= 0:
-			pass
-		else:
-			vuldomain+=url+"\n"
-			# print(vuldomain)
-			return True
-	except dns.resolver.NoAnswer:
-		vuldomain+=url+"\n"
-	except dns.exception.Timeout:
-		pass
+    global vuldomain
+    try:
+        A = str(dns.resolver.query(url,"txt").response)
+        if A.find("v=spf") >= 0:
+            pass
+        else:
+            vuldomain+=url+"\n"
+            # print(vuldomain)
+            return True
+    except dns.resolver.NoAnswer:
+        vuldomain+=url+"\n"
+    except dns.exception.Timeout:
+        pass
 
 
 #提醒
 def sendresult(vuldomain):
-	# vuldomain
-	ding={
-	    "msgtype": "text", 
-	    "text": {
-	        "content": "以下spf未配置，请检查!\n%s" % vuldomain
-	    }
-	}
-	res=requests.post(url=tokenurl,data=json.dumps(ding),headers=headers)
+    # vuldomain
+    ding={
+        "msgtype": "text", 
+        "text": {
+            "content": "以下spf未配置，请检查!\n%s" % vuldomain
+        }
+    }
+    res=requests.post(url=tokenurl,data=json.dumps(ding),headers=headers)
 
 
 if __name__ == '__main__':
-	if sys.argv == 1:
-		print("python spfcheck.py targets.txt")
-	else:
-		file=sys.argv[1]
-	for url in open(file) :
-		check_vul(url.strip())
-	if vuldomain.strip() != '':
-		sendresult(vuldomain)
-	else:
-		pass
+    if sys.argv == 1:
+        print("python spfcheck.py targets.txt")
+    else:
+        file=sys.argv[1]
+    for url in open(file) :
+        check_vul(url.strip())
+    if vuldomain.strip() != '':
+        sendresult(vuldomain)
+    else:
+        pass
